a2a1e67046a03c5393e3fc70cd5616e458d839ce
hillarymarler <Marler.Hillary@epa.gov>
2025-03-06 08:28:04 -0500

    added dont run to actions examples

--- R/EQ_Actions.R
+++ R/EQ_Actions.R
@@ -62,6 +62,7 @@
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #' # actions from OR established between fiscal years 2018 and 2020
 #' OR_actions_ex <- EQ_Actions(statecode = "OR", fisc_year_start = 2018, fisc_year_end = 2020,
 #'                             api_key = testkey)
@@ -69,6 +70,7 @@
 #' Region5_4B<- EQ_Actions(region = 5,
 #'               act_type = "4B Restoration Approach",
 #'               api_key = testkey)
+#'               }
 #'
 EQ_Actions <- function(api_key = NULL, act_agency = NULL, act_id = NULL, act_name = NULL,
                        act_type = NULL, au_name = NULL, auid = NULL, comp_date_end = NULL,
