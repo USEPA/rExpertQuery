db5dc350c6c150c235385b1a1d5e9af491e44757
hillarymarler <Marler.Hillary@epa.gov>
2025-05-09 14:34:41 -0400

    Delete notes.R

--- R/notes.R
+++
@@ -1,9 +0,0 @@
-# notes for questions to add from common questions
-
-#1 How many stream miles in STATE are impaired for USE?
-#2 What waters in STATE are impaired for USE due to PARAMETER?
-#3 What are all the TMDLs in STATE?
-#4 Which waters are new to CAT# in this cycle in STATE?
-#5 How many TMDL projects were approved so far in FY 2025 in REGION?
-
-
