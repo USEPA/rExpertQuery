3ccdcdf9b1284e5437ada218d8f7b37b34794b51
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-03-18 09:53:41 -0400

    bug fixes

--- R/EQ_DomainValues.R
+++ R/EQ_DomainValues.R
@@ -97,7 +97,7 @@ EQ_DomainValues <- function(domain = NULL) {
                                    package = "rExpertQuery", mustWork = TRUE)
 
         load(dv_filepath, envir = e)
-        eq_params <- e[["domain_values"]]
+        eq.params <- e[["domain_values"]]
         return(eq.params)
       }
   }
