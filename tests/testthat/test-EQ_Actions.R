b2580fc426080e90c39ed7574061be256777bcb1
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-04-16 14:04:58 -0400

    Update test-EQ_Actions.R

--- tests/testthat/test-EQ_Actions.R
+++ tests/testthat/test-EQ_Actions.R
@@ -8,4 +8,28 @@ httptest2::with_mock_dir("dir/RI-actions", {
     expect_equal(NCOL(EQ_Actions(statecode = "RI",
                                  api_key = .setEQKey())), 23)
   })
+
+  testthat::test_that("Actions returns expected column names", {
+
+    param.cw <- utils::read.csv(system.file("extdata", "EQParamsCrosswalk.csv",
+                                            package = "rExpertQuery"
+    ))
+
+    expected <- c("objectId", "region", "state", "organizationType",
+                  "organizationId", "organizationName", "waterType",
+                  "parameterGroup", "parameter", "actionType", "actionId",
+                  "actionName", "actionAgency", "inIndianCountry",
+                  "includeInMeasure", "completionDate", "assessmentUnitId",
+                  "assessmentUnitName", "fiscalYearEstablished",
+                  "locationDescription", "waterSize", "waterSizeUnits",
+                  "planSummaryLink")
+
+    actual <- names(EQ_Actions(statecode = "RI",
+                         api_key = .setEQKey()))
+
+    length.diff <- length(setdiff(expected, actual))
+
+    testthat::expect_equal(length.diff, 0)
+
+  })
 })
