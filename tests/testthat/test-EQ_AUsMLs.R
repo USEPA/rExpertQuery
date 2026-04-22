0c4828ac6d1b232b22e37bd5b49e9a0b67c9491c
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-04-22 08:30:09 -0400

    add ausmls test

---
+++ tests/testthat/test-EQ_AUsMLs.R
@@ -0,0 +1,37 @@
+httptest2::with_mock_dir("dir/MT-ausmls-kleinschmidtcreek", {
+  testthat::test_that("EQ_AUsMLs returns expected number of rows", {
+    expect_equal(NROW(EQ_AUsMLs(org_id = "MTDEQ",
+                                au_name = "Kleinschmidt Creek",
+                                api_key = .setEQKey())
+    ), 16)
+  })
+
+  testthat::test_that("EQ_AUsMLs returns expected number of columns", {
+    expect_equal(NCOL(EQ_AUsMLs(org_id = "MTDEQ",
+                                au_name = "Kleinschmidt Creek",
+                                api_key = .setEQKey())
+    ), 21)
+  })
+
+  testthat::test_that("EQ_AUsMLs returns expected column names", {
+
+    expected <- c("objectId", "region", "state", "organizationType",
+                  "organizationId", "organizationName", "waterType",
+                  "useClassName", "monitoringLocationId",
+                  "monitoringLocationOrgId", "assessmentUnitId",
+                  "assessmentUnitName", "assessmentUnitStatus",
+                  "reportingCycle", "cycleId", "locationDescription",
+                  "monitoringLocationDataLink", "sizeSource", "sourceScale",
+                  "waterSize", "waterSizeUnits"  )
+
+    actual <- names(EQ_AUsMLs(org_id = "MTDEQ",
+                              au_name = "Kleinschmidt Creek",
+                              api_key = .setEQKey())
+    )
+
+    length.diff <- length(setdiff(expected, actual))
+
+    testthat::expect_equal(length.diff, 0)
+
+  })
+})
