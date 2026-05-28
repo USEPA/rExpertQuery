bb0d0d600f114c2e80bed8124866aab0daa06be3
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-05-28 10:03:56 -0400

    Update redact.R

--- inst/httptest2/redact.R
+++ inst/httptest2/redact.R
@@ -1,4 +1,10 @@
-set_redactor(function (x) {
-  gsub_response(x, "https://api.epa.gov/expertquery/api/attains/", "epa/")
+# inst/httptest2/redact.R
+# Always use fully qualified calls to httptest2 functions here.
+httptest2::set_redactor(function(x) {
+  x |>
+    # Example replacements; adjust to your API/paths
+    httptest2::gsub_response("https://api.epa.gov/expertquery/api/attains/", "epa/") |>
+    httptest2::gsub_response("https://api.epa.gov/expertquery/api/", "epa/") |>
+    # Redact Authorization headers, query params, etc., as needed
+    httptest2::redact_authorization()
 })
-
