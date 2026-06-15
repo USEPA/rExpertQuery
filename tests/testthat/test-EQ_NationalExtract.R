0e95c2cc9922f37059c2f64f3a11d8cec685fb51
hillarymarler <152432687+hillarymarler@users.noreply.github.com>
2026-06-15 14:00:46 -0400

    add helpers to resolve CI failures

--- tests/testthat/test-EQ_NationalExtract.R
+++ tests/testthat/test-EQ_NationalExtract.R
@@ -1,14 +1,8 @@
-testthat::test_that("EQ_NationalExtract returns all expected columns for Actions (CSV from RDS)", {
+testthat::test_that("EQ_NationalExtract returns all expected columns for Actions", {
   testthat::skip_if_not_installed("data.table")
 
   # load mock actions
-  actions_df_raw <- readRDS(testthat::test_path("htt2", "NATact", "actions.rds"))
-
-  # write to temp csv
-  tmp_dir <- tempfile("ne_actions_")
-  dir.create(tmp_dir, recursive = TRUE, showWarnings = FALSE)
-  actions_csv <- file.path(tmp_dir, "actions.csv")
-  data.table::fwrite(actions_df_raw, actions_csv)
+  actions_csv <- make_raw_csv_from_rds("actions", c("htt2","NATact","actions.rds"))
 
   # set up override list
   overrides <- list()
@@ -20,17 +14,10 @@ testthat::test_that("EQ_NationalExtract returns all expected columns for Actions
       function(...) tempfile(fileext = ".zip")  # return a dummy zip path
     )
   }
-  # make unzip return the temp csv path
-  overrides[[length(overrides) + 1L]] <- .local_override(
-    "utils", "unzip",
-    function(zipfile, exdir = tempdir(), ...) {
-      dest <- file.path(exdir, basename(actions_csv))
-      dir.create(dirname(dest), recursive = TRUE, showWarnings = FALSE)
-      file.copy(actions_csv, dest, overwrite = TRUE)
-      dest
-    }
-  )
 
+  overrides[[length(overrides) + 1L]] <- .local_override(
+    "utils", "unzip", mock_unzip_returning(actions_csv)
+  )
   # restore all overrides after this test
   on.exit(.local_restore(overrides), add = TRUE)
 
@@ -57,13 +44,7 @@ testthat::test_that("EQ_NationalExtract returns all expected columns for TMDLs",
   testthat::skip_if_not_installed("data.table")
 
   # load mock actions
-  tmdls_df_raw <- readRDS(testthat::test_path("htt2", "NATtmdl", "tmdls.rds"))
-
-  # write to temp csv
-  tmp_dir <- tempfile("ne_tmdls_")
-  dir.create(tmp_dir, recursive = TRUE, showWarnings = FALSE)
-  tmdls_csv <- file.path(tmp_dir, "tmdls.csv")
-  data.table::fwrite(tmdls_df_raw, tmdls_csv)
+  tmdls_csv <- make_raw_csv_from_rds("tmdl", c("htt2","NATtmdl","tmdls.rds"))
 
   # set up override list
   overrides <- list()
@@ -75,15 +56,9 @@ testthat::test_that("EQ_NationalExtract returns all expected columns for TMDLs",
       function(...) tempfile(fileext = ".zip")  # return a dummy zip path
     )
   }
-  # make unzip return the temp csv path
+
   overrides[[length(overrides) + 1L]] <- .local_override(
-    "utils", "unzip",
-    function(zipfile, exdir = tempdir(), ...) {
-      dest <- file.path(exdir, basename(tmdls_csv))
-      dir.create(dirname(dest), recursive = TRUE, showWarnings = FALSE)
-      file.copy(tmdls_csv, dest, overwrite = TRUE)
-      dest
-    }
+    "utils", "unzip", mock_unzip_returning(tmdls_csv)
   )
 
   # restore all overrides after this test
